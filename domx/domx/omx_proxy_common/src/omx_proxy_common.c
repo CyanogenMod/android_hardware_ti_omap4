98182c2b5765b4c9f03264bfedbd83b4a1b086d2
Ziyan <jaraidaniel@gmail.com>
2015-10-16 21:30:42 +0200

    domx: Fix GetParameter crashing with OMX_IndexParamConsumerUsageBits
    Thanks to Kyle Repinski <repinski23@gmail.com> for pinpointing the issue,
    I just ifdef'd it and modified the return value to OMX_ErrorNotImplemented.
    
    Since OMX_IndexParamConsumerUsageBits is fairly new, we're
    assuming none of the current omap4 ducatis implement it.
    
    Change-Id: Ib1e26c8be36d90ef1624699a5d4107ae25794951

--- domx/domx/omx_proxy_common/src/omx_proxy_common.c
+++ domx/domx/omx_proxy_common/src/omx_proxy_common.c
@@ -64,6 +64,7 @@
 #include "OMX_TI_Common.h"
 #include "OMX_TI_Index.h"
 #include "OMX_TI_Core.h"
+#include "OMX_IndexExt.h"
 /*-------program files ----------------------------------------*/
 #include "omx_proxy_common.h"
 #include "omx_rpc.h"
@@ -1483,6 +1484,12 @@ OMX_ERRORTYPE __PROXY_GetParameter(OMX_IN OMX_HANDLETYPE hComponent,
 			     ptBufDescParam->eBufferType = OMX_TI_BufferTypeVirtual2D;
 		     }
 		     break;
+
+#ifdef ANDROID_API_MM_OR_LATER
+		case OMX_IndexParamConsumerUsageBits:
+			eError = OMX_ErrorNotImplemented;
+			goto EXIT;
+#endif
 
 		default:
 		{
