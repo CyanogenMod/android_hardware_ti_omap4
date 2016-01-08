c045552783fb84f25a004b9a3eced6cb80bec30a
Michael Gernoth <michael@gernoth.net>
2016-01-08 20:45:43 +0100

    gralloc: add optional gralloc-wrapper
    This provides an optional gralloc-wrapper to filter out unsupported
    usage-bits before they reach the real gralloc-blob. Currently this
    only applies to GRALLOC_USAGE_CURSOR which allows mouse-cursors to
    be visible again.
    
    To use this replace gralloc.omap4.so in your device makefile with
    gralloc.BOARD_NAME and define BOARD_GRALLOC_HMI_SIZE in your
    BoardConfig.mk with the hex-value (prefix 0x) provided by:
    $ arm-linux-androideabi-nm -S -D $OUT/system/vendor/lib/hw/gralloc.omap4.so | grep HMI | cut -d' ' -f 2
    
    Change-Id: I88baf92de9a74939b65f89e9628b62be88cfaede

---
+++ gralloc/Android.mk
@@ -0,0 +1,27 @@
+###
+### Wrapper for TI gralloc
+###
+
+# To find your correct BOARD_GRALLOC_HMI_SIZE run:
+# arm-linux-androideabi-nm -S -D $OUT/system/vendor/lib/hw/gralloc.omap4.so | grep HMI | cut -d' ' -f 2
+
+ifneq ($(BOARD_GRALLOC_HMI_SIZE),)
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := gralloc.$(TARGET_DEVICE)
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/hw/
+LOCAL_SRC_FILES := wrapper.c
+
+LOCAL_CFLAGS := -DREAL_GRALLOC=\"gralloc.omap$(TARGET_BOARD_OMAP_CPU).so\"
+LOCAL_CFLAGS += -DGRALLOC_HMI_SIZE=$(BOARD_GRALLOC_HMI_SIZE)
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
---
+++ gralloc/wrapper.c
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "ti_gralloc_wrapper"
+/* #define LOG_NDEBUG 0 */
+
+#include <errno.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/mman.h>
+
+#include <cutils/log.h>
+
+#include <hardware/hardware.h>
+#include <hardware/gralloc.h>
+
+/* These bits should be removed from usage */
+#define GRALLOC_REMOVE_USAGE_MASK       (GRALLOC_USAGE_CURSOR)
+
+/* Padded module structure */
+typedef struct IMG_gralloc_module_t
+{
+    gralloc_module_t base;
+    unsigned char pad[GRALLOC_HMI_SIZE];
+} IMG_gralloc_module_t;
+
+static void *dso_handle = NULL;
+
+/* Function pointers to HAL */
+int (*real_gralloc_alloc)(struct alloc_device_t* dev, int w, int h, int format, int usage, buffer_handle_t* handle, int* stride) = NULL;
+
+/* Pointers to data structures from HAL */
+static IMG_gralloc_module_t *real_gralloc_module = NULL;
+static alloc_device_t *real_gralloc_device = NULL;
+
+int wrapper_lock(struct gralloc_module_t const* module, buffer_handle_t handle, int usage, int l, int t, int w, int h, void** vaddr)
+{
+    usage &= ~(GRALLOC_REMOVE_USAGE_MASK);
+
+    return real_gralloc_module->base.lock(module, handle, usage, l, t, w, h, vaddr);
+}
+
+int wrapper_alloc(struct alloc_device_t* dev, int w, int h, int format, int usage, buffer_handle_t* handle, int* stride)
+{
+    usage &= ~(GRALLOC_REMOVE_USAGE_MASK);
+
+    return real_gralloc_alloc(dev, w, h, format, usage, handle, stride);
+}
+
+static int wrapper_open(const hw_module_t* module, const char* name, hw_device_t** device);
+
+static struct hw_module_methods_t wrapper_module_methods = {
+    .open = wrapper_open,
+};
+
+IMG_gralloc_module_t HAL_MODULE_INFO_SYM = {
+    .base = {
+        .common = {
+            .tag = HARDWARE_MODULE_TAG,
+            .version_major = 0,
+            .version_minor = 1,
+            .id = GRALLOC_HARDWARE_MODULE_ID,
+            .name = "TI gralloc wrapper",
+            .author = "The CyanogenMod Project (Michael Gernoth)",
+            .methods = &wrapper_module_methods
+        },
+    },
+};
+
+static int wrapper_open(const hw_module_t* module, const char* name, hw_device_t** device)
+{
+    int ret;
+
+    if (!real_gralloc_module) {
+        return -EINVAL;
+    }
+
+    ((IMG_gralloc_module_t*)module)->base.common.dso = dso_handle;
+
+    ret = real_gralloc_module->base.common.methods->open(module, name, device);
+    if (ret != 0) {
+        ALOGE("wrapper_open: couldn't open");
+        return ret;
+    }
+
+    real_gralloc_alloc = ((alloc_device_t*)(*device))->alloc;
+
+    ((alloc_device_t*)(*device))->alloc = wrapper_alloc;
+
+    return ret;
+}
+
+__attribute__((constructor)) void overload()
+{
+    unsigned int i;
+
+    ALOGI("Initializing wrapper for TI's gralloc");
+
+    dso_handle = dlopen("/system/vendor/lib/hw/" REAL_GRALLOC, RTLD_NOW);
+    if (dso_handle == NULL) {
+        char const *err_str = dlerror();
+        ALOGE("wrapper_open: %s", err_str ? err_str : "unknown");
+        return;
+    }
+
+    const char *sym = HAL_MODULE_INFO_SYM_AS_STR;
+    real_gralloc_module = (IMG_gralloc_module_t*)dlsym(dso_handle, sym);
+    if (real_gralloc_module == NULL) {
+        ALOGE("wrapper_open: couldn't find symbol %s", sym);
+        dlclose(dso_handle);
+        dso_handle = NULL;
+        return;
+    }
+
+    memcpy(&HAL_MODULE_INFO_SYM, real_gralloc_module, GRALLOC_HMI_SIZE);
+
+    ALOGI("Impersonating %s by %s", HAL_MODULE_INFO_SYM.base.common.name, HAL_MODULE_INFO_SYM.base.common.author);
+
+    HAL_MODULE_INFO_SYM.base.common.methods = &wrapper_module_methods;
+    HAL_MODULE_INFO_SYM.base.lock = wrapper_lock;
+}
