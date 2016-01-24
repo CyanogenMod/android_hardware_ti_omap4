ed1c1382843a84c0510bd6e3aaa9c5b3229f706a
Ziyan <jaraidaniel@gmail.com>
2016-01-24 13:01:49 +0100

    sgxfreq: set correct frequency limit on omap4460/4470
    Commit 5e398ba changes how the initial frequency limit is selected: instead of
    selecting the highest avaliable frequency, it hardcodes it to SYS_SGX_CLOCK_SPEED,
    which is defined as 307200000 Hz for SGX540_120. However, omap4460/4470 devices
    use 384000000 Hz, so we end up underclocking them.
    
    Instead of hardcoding these frequencies, let's just use them as a reference
    frequency, and use the first frequency that is less than or equal to the correct
    reference frequency. This way, we can ensure a safe frequency set even on kernels
    with custom SGX clock frequencies enabled.
    
    Change-Id: Ic55be55dbb5c9d4a3807096209cdf07bbbdca719

--- pvr-source/services4/system/omap4/sgxfreq.c
+++ pvr-source/services4/system/omap4/sgxfreq.c
@@ -208,6 +208,29 @@ static const struct attribute *sgxfreq_attributes[] = {
 
 /************************ end sysfs interface ************************/
 
+static unsigned long __sgxfreq_get_max_safe_freq(void)
+{
+	int i;
+	unsigned long reference_freq = SYS_SGX_CLOCK_SPEED;
+	unsigned long safe_freq;
+
+	if (cpu_is_omap446x()
+#ifdef cpu_is_omap447x
+		|| cpu_is_omap447x()
+#endif
+		) {
+		reference_freq = 384000000;
+	}
+
+	for (i = sfd.freq_cnt - 1; i >= 0; i--) {
+		safe_freq = sfd.freq_list[i];
+		if (safe_freq <= reference_freq)
+			return safe_freq;
+	}
+
+	return safe_freq;
+}
+
 static void __set_freq(void)
 {
 	unsigned long freq;
@@ -334,7 +357,7 @@ int sgxfreq_init(struct device *dev)
 	rcu_read_unlock();
 
 	mutex_init(&sfd.freq_mutex);
-	sfd.freq_limit = SYS_SGX_CLOCK_SPEED;
+	sfd.freq_limit = __sgxfreq_get_max_safe_freq();
 	sgxfreq_set_freq_request(sfd.freq_list[sfd.freq_cnt - 1]);
 	sfd.sgx_data.clk_on = false;
 	sfd.sgx_data.active = false;
